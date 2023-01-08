c01cedf01c08054c8d0e4ea1f7501755e1d68095
Tobias Linton <tobias.linton@edgeware.tv>
2023-01-08 18:26:56 -0300

    Initial day5-1.hs

---
+++ day5/app/day5-1.hs
@@ -0,0 +1,59 @@
+import qualified Data.Stack as St
+import Data.Vector ((!?))
+import qualified Data.Vector as Vec
+import qualified Text.Parsec as Parsec
+import qualified Data.Char as Parsec
+
+type Stacks = Vec.Vector (St.Stack Char)
+
+data Token = Empty
+    | Crate { id :: Char }
+    | Move { count :: Int, src :: Int, dst :: Int }
+    | Col { idx :: Int }
+    deriving (Eq, Show)
+
+createStacks :: Stacks
+createStacks = Vec.empty
+
+parseLine :: String -> [Token]
+parseLine line =
+    case Parsec.parse parser "" line of
+        Left _ -> []
+        Right result -> result
+    where
+        empty = Parsec.count 3 Parsec.space >> return Empty
+        crate = do
+            Parsec.char '['
+            crateId <- Parsec.anyChar
+            Parsec.char ']'
+            return $ Crate crateId
+        col = do
+            idx <- Parsec.many Parsec.digit
+            return $ Col (read idx)
+        move = do
+            Parsec.string "move "
+            count <- Parsec.many Parsec.digit
+            Parsec.string " from "
+            src <- Parsec.many Parsec.digit
+            Parsec.string " to "
+            dst <- Parsec.many Parsec.digit
+            return $ Move (read count) (read src) (read dst)
+        parser = Parsec.choice [empty, crate, col, move] `Parsec.sepBy` Parsec.space
+
+pushData :: Stacks -> [Maybe Char] -> Stacks
+pushData stacks newData =
+    Vec.fromList $ zipWith updateStack [0..] newData
+    where
+        updateStack id value = case stacks !? id of
+            Just s -> pushValue s value
+            Nothing -> pushValue St.stackNew value
+        pushValue stack (Just value) = St.stackPush stack value
+        pushValue stack Nothing = stack
+
+main :: IO ()
+main = do
+    instructions <- getContents
+    let stacks = createStacks
+    --print $ pushData (pushData stacks [Just 'A', Nothing, Just 'C']) [Just 'B', Just 'z', Just 'F']
+    --print $ parseLine "[A] [B]"
+    print $ map parseLine (lines instructions)
